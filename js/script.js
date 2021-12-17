dd14d25a269db4ec4a690d81521a0a7f23e84bbf
Sanjar Abduraimov <sanjarbekweb@gmail.com>
2021-12-18 00:38:05 +0500

    :recycle: navigation animation  added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-window.addEventListener('DOMContentLoaded' , function(){
-    window.addEventListener('scroll',function(){
-        const header=document.querySelector('header')
-        header.classList.toggle('sticky', window.scrollY>0)
-    });
-    const menuBtn=document.querySelector('.menu-btn');
-    const navigation=document.querySelector('navigation');
-
-
-    menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('active')
-        navigation.classList.toggle('active')
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector(".menu-btn");
+  const navigation = document.querySelector(".navigation");
+  const header = document.querySelector("header");
+  menuBtn.onclick = () => {
+    navigation.classList.toggle("active");
+  };
+  let currentCordinate = 0;
+  document.addEventListener("scroll", () => {
+    header.classList.toggle("active", scrollY > 0);
+    // if (scrollY > currentCordinate) {
+    //   header.classList.remove("pinned");
+    //   header.classList.add("unpinned");
+    // } else {
+    //   header.classList.remove("unpinned");
+    //   header.classList.add("pinned");
+    // }
+    // currentCordinate = scrollY;
+  });
+});
