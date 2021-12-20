cb2ff7ff90107752b01c446626d9849fbe6a185b
Lochinbek <lochinbekabduvoitov8@gmail.com>
2021-12-20 12:20:08 +0300

    New edits

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll',function(){
+    const header=document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0)
+  });
   const menuBtn = document.querySelector(".menu-btn");
   const navigation = document.querySelector(".navigation");
   const header = document.querySelector("header");
   menuBtn.onclick = () => {
     navigation.classList.toggle("active");
   };
-  let currentCordinate = 0;
-  document.addEventListener("scroll", () => {
-    header.classList.toggle("active", scrollY > 0);
-    // if (scrollY > currentCordinate) {
-    //   header.classList.remove("pinned");
-    //   header.classList.add("unpinned");
-    // } else {
-    //   header.classList.remove("unpinned");
-    //   header.classList.add("pinned");
-    // }
-    // currentCordinate = scrollY;
-  });
+  
+  const typed=document.querySelector('.typed');
+  typed={
+    strings:[
+      'Lochinbek ',
+      'Abduvoitov'
+    ],
+    typedSpeed:60,
+    startDelay:500,
+    backSpeed:40,
+    loop:true,
+    loopCount:Infinity,
+  };
+ 
+
+  
 });
