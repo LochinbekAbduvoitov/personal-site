ed07bd95968697e61d102dc2f0fea5873277e189
Lochinbek <lochinbekabduvoitov8@gmail.com>
2022-01-19 19:21:26 +0500

    Loader add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 window.addEventListener('DOMContentLoaded', function () {
 
+const loader=document.querySelector(".loader");
+  
+  setTimeout(function(){
+    loader.style.opacity=0;
+    setTimeout(function(){
+      loader.style.display="none"
+    },2000)
+  },3000)
+
 
   window.addEventListener('scroll',function(){
     const header=document.querySelector('header');
