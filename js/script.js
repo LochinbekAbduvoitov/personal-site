6ed503f682fad92aecf36675559a7fd9adf74425
Lochinbek <lochinbekabduvoitov8@gmail.com>
2022-01-22 19:22:30 +0500

    Loader page add

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const loader=document.querySelector(".loader");
     loader.style.opacity=0;
     setTimeout(function(){
       loader.style.display="none"
-    },2000)
-  },3000)
+    },1500)
+  },2000)
 
 
   window.addEventListener('scroll',function(){
@@ -17,7 +17,7 @@ const loader=document.querySelector(".loader");
   const menuBtn = document.querySelector(".menu-btn");
   const navigation = document.querySelector(".navigation");
   const header = document.querySelector("header");
-  const navigationItems=document.querySelectorAll('.navigation a');
+  const navigationItems=document.querySelectorAll('.navigation  a');
   menuBtn.onclick = () => {
     navigation.classList.toggle("active");
   };
@@ -34,7 +34,7 @@ const loader=document.querySelector(".loader");
   let options={
     strings: [
       'Lochinbek ',
-      'Abduvoitov','Junior developer'
+      'Abduvoitov','Junior'
     ],
     typeSpeed:70,
     startDelay:500,
@@ -79,4 +79,62 @@ const loader=document.querySelector(".loader");
     }
 
   })
+  // My work SECTION CARUSEL
+
+  const content=document.querySelector(".slide-content");
+  const card=document.querySelectorAll(".slide-card");
+  const prev=document.querySelector(".angle-box-left");
+  const next=document.querySelector(".angle-box-right");
+
+  content.style.overflow="hidden"
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //Botga xabar yuborish
+
+let telegram_bot_id = "5243118463:AAGuCr1mfCqEFmyAUZMNn34zVM-RBldFefM";
+
+let chat_id = 1652305676; 
+let u_name, email, message;
+let ready = function() {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nIzoh: " + message;
+};
+let sendtelegram = function() {
+    ready();
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
+
 });
